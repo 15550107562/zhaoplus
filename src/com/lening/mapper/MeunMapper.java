9feb0175704a46aee72dc509a3fbfbc36b9c0c34
zhao <2483877607@qq.com>
2021-04-16 15:22:23 +0800

    这是第一个

---
+++ src/com/lening/mapper/MeunMapper.java
@@ -0,0 +1,30 @@
+package com.lening.mapper;
+
+import com.lening.entity.MeunBean;
+import com.lening.entity.MeunBeanExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface MeunMapper {
+    long countByExample(MeunBeanExample example);
+
+    int deleteByExample(MeunBeanExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(MeunBean record);
+
+    int insertSelective(MeunBean record);
+
+    List<MeunBean> selectByExample(MeunBeanExample example);
+
+    MeunBean selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") MeunBean record, @Param("example") MeunBeanExample example);
+
+    int updateByExample(@Param("record") MeunBean record, @Param("example") MeunBeanExample example);
+
+    int updateByPrimaryKeySelective(MeunBean record);
+
+    int updateByPrimaryKey(MeunBean record);
+}
